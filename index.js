c6530923742d0dc38b18323d333a1e05746c79cd
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 14:59:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+customerName = "bob";
+
+function upperCaseCustomerName(){
+
+customerName = customerName.toUpperCase();
+
+
+
+}
+
+function setBestCustomer(){
+
+bestCustomer = 'not bob';
+
+
+}
+
+function overwriteBestCustomer(){
+
+bestCustomer = 'maybe bob';
+
+
+}
+
+function changeLeastFavoriteCustomer(customerName){
+
+customerName = customerName.toUpperCase();
+
+return customerName;
+
+}
